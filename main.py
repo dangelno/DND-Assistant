ceba5aa9f2473976b29c405ae8f3ae2fbb6933be
dangelno <ddsidiropulo08@gmail.com>
2025-02-16 14:06:01 +0100

    First commit: added project files

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+import openai
+from fastapi import FastAPI, Request, Query
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+# Подключаем шаблоны и статику
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "title": "D&D Assistant"})
+
+@app.get("/pers")
+async def home(request: Request):
+    return templates.TemplateResponse("pers.html", {"request": request, "title": "D&D pers create"})
+
+@app.get("/saved")
+async def home(request: Request):
+    return templates.TemplateResponse("saved.html", {"request": request, "title": "D&D pers saved"})
+
+@app.get("/adventure")
+async def home(request: Request):
+    return templates.TemplateResponse("adventure.html", {"request": request, "title": "D&D adventure"})
+
+@app.get("/dice")
+async def home(request: Request):
+    return templates.TemplateResponse("dice.html", {"request": request, "title": "D&D dice"})
